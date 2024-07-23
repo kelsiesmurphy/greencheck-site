b72b29d1e255394509d8f03f33574a5a6d09d132
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-07-23 09:33:01 +0100

    Updating footer etc.

--- src/routes/(components)/MobileSheet.svelte
+++ src/routes/(components)/MobileSheet.svelte
@@ -6,6 +6,21 @@
 	import Logomark from '$lib/assets/Logomark.svg';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import { sendGumroadLog } from '$lib/utils/logs';
+
+	const listItems = [
+		{
+			title: 'Instructions',
+			link: '/instructions'
+		},
+		{
+			title: 'Manage License',
+			link: '/manage-license'
+		},
+		{
+			title: 'Privacy Policy',
+			link: '/privacy'
+		}
+	];
 </script>
 
 <Sheet.Root>
@@ -18,20 +33,31 @@
 				<a href="/"><img src={Logomark} alt="Greencheck logo" /></a>
 			</Sheet.Title>
 		</Sheet.Header>
-		<div class="flex flex-col gap-3">
-			<Button
-				href={content.chromeStoreLink}
-				on:click={() => sendGumroadLog('chrome', 'Get Basic for free in Mobile Sheet')}
-				target="_blank"
-				variant="secondary"><Globe class="mr-2 h-4 w-4" />Get Basic for free</Button
-			>
-			<Button
-				href={content.gumroadLink}
-				target="_blank"
-				on:click={() => sendGumroadLog('gumroad', 'Gumroad Button in Mobile Sheet')}
-			>
-				<ShoppingCart class="mr-2 h-4 w-4" />Get Plus for £{content.price}
-			</Button>
-		</div>
+		<ul class="space-y-4">
+			{#each listItems as value}
+				<li>
+					<Button class="w-full" href={value.link} variant="ghost">{value.title}</Button>
+				</li>
+			{/each}
+			<li>
+				<Button
+					class="w-full"
+					href={content.chromeStoreLink}
+					on:click={() => sendGumroadLog('chrome', 'Get Basic for free in Mobile Sheet')}
+					target="_blank"
+					variant="secondary"><Globe class="mr-2 h-4 w-4" />Get Basic for free</Button
+				>
+			</li>
+			<li>
+				<Button
+					class="w-full"
+					href={content.gumroadLink}
+					target="_blank"
+					on:click={() => sendGumroadLog('gumroad', 'Gumroad Button in Mobile Sheet')}
+				>
+					<ShoppingCart class="mr-2 h-4 w-4" />Get Plus for £{content.price}
+				</Button>
+			</li>
+		</ul>
 	</Sheet.Content>
 </Sheet.Root>
